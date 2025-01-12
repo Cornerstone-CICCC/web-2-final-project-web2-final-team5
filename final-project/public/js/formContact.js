b58e0cc559e8728cffcced4f14214f8f0aa7218c
Majito Brinkitos <mcvef2013@gmail.com>
2025-01-11 21:24:08 -0800

    final design contact

--- final-project/public/js/formContact.js
+++ final-project/public/js/formContact.js
@@ -15,16 +15,30 @@ contactForm.addEventListener('submit', function(event) {
         phone: document.getElementById('phone').value, 
         email: document.getElementById('email').value, 
         service: document.getElementById('services').value, 
-        appointmentDate: document.getElementById('appointmentDate').value, message: document.getElementById('message').value,
+        appointmentDate: document.getElementById('appointmentDate').value, 
+        message: document.getElementById('message').value,
     };
 
     //Sending email using EmailJS
     emailjs.send('service_4io1hjs', 'contact_form', formData)
     .then((response) => {
         console.log('SUCCESS!', response.status, response.text);
-        alert('Your message has been sent successfully!');
+        displayMessage('Your message has been sent successfully!', 'success');
     }, (error) => {
         console.log('FAILED...', error); 
-        alert('Failed to send your message. Please try again.');
+        displayMessage('Failed to send your message. Please try again.', 'error');
     });
 });
+
+    //Function to display messages
+    function displayMessage(message, type) {
+        const messageContainer = document.getElementById('messageContainer'); 
+        const messageElement = document.createElement('div'); 
+        messageElement.textContent = message; 
+        messageElement.classList.add(type === 'success' ? 'success-message' : 'error-message'); 
+        messageContainer.innerHTML = ''; // Clear previous messages 
+        messageContainer.appendChild(messageElement);
+
+    }
+
+
